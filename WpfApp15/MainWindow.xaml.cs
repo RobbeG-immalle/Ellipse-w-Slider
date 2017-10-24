1d8c63a25b79cde6bf6bae5ee83480b955ca3141
robbe.gillis <robbe.gillis@immalle.be>
2017-10-24 09:16:50 +0200

    Slider

--- WpfApp15/MainWindow.xaml.cs
+++ WpfApp15/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace WpfApp15
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Ellipse ellipse;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            vertSlider.Minimum = 0;
+            vertSlider.Maximum = mijnCanvas.Height;
+
+            horizSlider.Minimum = 0;
+            horizSlider.Maximum = mijnCanvas.Width;
+
+            vertLabel.Content = Convert.ToString(vertSlider.Value);
+            horizLabel.Content = Convert.ToString(horizSlider.Value);
+
+            CreateEllipse();
+        }
+
+        private void vertSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int vert = Convert.ToInt32(vertSlider.Value);
+            vertLabel.Content = Convert.ToString(vert);
+
+            UpdateEllipse();
+        }
+
+        private void horizSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+
+        {
+            int horiz = Convert.ToInt32(horizSlider.Value);
+            horizLabel.Content = Convert.ToString(horiz);
+
+            UpdateEllipse();
+        }
+
+
+        private void CreateEllipse()
+        {
+            ellipse = new Ellipse();
+            ellipse.Width = horizSlider.Value;
+            ellipse.Height = vertSlider.Value;
+            ellipse.Stroke = new SolidColorBrush(Colors.Red);
+            ellipse.Fill = new SolidColorBrush(Colors.Beige);
+            ellipse.Margin = new Thickness(0, 0, 0, 0);
+            mijnCanvas.Children.Add(ellipse);
+        }
+        private void UpdateEllipse()
+        {
+            ellipse.Width = horizSlider.Value;
+            ellipse.Height = vertSlider.Value;
         }
     }
-}
+ }
+
